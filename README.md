81e2ddfdad488b35b6fe198dcd54844e5b2b1043
qpid <sauravsachidanand@gmail.com>
2015-11-06 23:30:36 +0530

    ~

---
+++ README.md
@@ -0,0 +1,8 @@
+# Rusty Astronomy (work in progress)
+This repository will contain highly accurate and efficient astronomical algorithms for calculating interesting things about bodies in our solar system.
+
+## Available things
+* Finding the Julian Day
+
+## References
+* *Astronomical Algorithms, Second Edition by Jean Meeus*
---
+++ time/julian_day.rs
@@ -0,0 +1,41 @@
+fn main() {
+    let y = 2000;
+    let m = 1;
+    let d = 1.0;
+    println!("{}", julian_day(y, m, d)); // should be 2451179.5
+}
+
+// returns the decimal day for a given (day, hour, minute, second)
+fn decimal_day(d: i32, h: i32, m: i32, s: f64) -> f64 {
+    (d as f64) +
+    (h as f64) / 24.0 +
+    (m as f64) / 60.0 +
+    s / 60.0
+}
+
+// returns the julian day for a given (year, month, decimal day)
+// assumes currently that the given date is from the Gregorian calendar
+fn julian_day(mut y: i32, mut m: i16, d: f64) -> f64 {
+
+    if m == 1 || m == 2 {
+        y = y - 1;
+        m = m + 12;
+    }
+    let a: i64 = int((y as f64) / 100.0);
+    let b: i64 = 2 - a + int(a as f64 / 4.0);
+
+    (int(365.25 * ((y as f64) + 4716.0)) as f64) +
+    (int(30.6001 * ((m as f64) + 1.0)) as f64) +
+    (d as f64) +
+    (b as f64) -
+    1524.5
+
+}
+
+// returns the largest integer less than or equal to (x)
+fn int(x: f64) -> i64 {
+    if x < 0_f64 {
+        return -1 + x as i64;
+    }
+    x as i64
+}
