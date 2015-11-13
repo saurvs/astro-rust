a26556b357bd207d144ad7f88d560be3642d0a41
qpid <sauravsachidanand@gmail.com>
2015-11-14 01:08:23 +0530

    Updated comments in angle.rs

--- src/angle.rs
+++ src/angle.rs
@@ -1,4 +1,13 @@
-// converts angles expressed with degrees, minutes seconds to pure degrees
+/*
+
+    Returns the angle expressed in degrees only.
+    -----------------------------------------------------------------
+        d: Degrees
+        m: Minute
+        s: Second
+
+*/
+
 pub fn pure_degrees(d: f64, mut m: f64, mut s: f64) -> f64 {
     if d < 0.0 {
         m = -1.0 * m.abs();
@@ -6,6 +15,14 @@ pub fn pure_degrees(d: f64, mut m: f64, mut s: f64) -> f64 {
     }
     d + (m / 60.0) + (s / 3600.0)
 }
+
+/*
+
+    Returns the equivalent angle in the range 0 - 360 degrees.
+    -----------------------------------------------------------------
+        angle: The angle in degrees
+
+*/
 
 pub fn limited_to_360(angle: f64) -> f64 {
     let n = (angle / 360.0) as i64;
@@ -15,6 +32,16 @@ pub fn limited_to_360(angle: f64) -> f64 {
     }
     limited_angle
 }
+
+/*
+
+    Checks wether an angle in degrees is sufficiently "small", for an
+    an arbritrary definition of "small", chosen for adequate purposes
+    in this library.
+    -----------------------------------------------------------------
+        angle: The angle in degrees
+
+*/
 
 pub fn small_angle(angle: f64) -> bool {
     if angle < 0.003 { return true; }
