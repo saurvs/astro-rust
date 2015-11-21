54dc07c28579f805c8d57f253dbd67454d823af6
qpid <sauravsachidanand@gmail.com>
2015-11-21 14:33:16 +0530

    Updated usage of date structs in usage.rs

--- examples/usage.rs
+++ examples/usage.rs
@@ -14,7 +14,8 @@ fn main() {
     let moon_landing = time::greg_date {
         y: 1969,
         m: 7,
-        d: time::decimal_day(moon_landing_day)
+        d: time::decimal_day(moon_landing_day),
+        t: time::calendar_type::greogrian
     };
     println!("{}", julian_day::julian_day(moon_landing));
 
@@ -31,12 +32,5 @@ fn main() {
     /* Prints the ecliptic coordinates of the star Pollux given its equatorial coordinates */
     println!("{}", coordinates::ecl_long(116.328942_f64.to_radians(), 28.026183_f64.to_radians(), coordinates::oblq_ecl_2000()).to_degrees());
     println!("{}", coordinates::ecl_lat(116.328942_f64.to_radians(), 28.026183_f64.to_radians(), coordinates::oblq_ecl_2000()).to_degrees());
-
-    let (x,y, z) = elliptic_motion::geocentric_position(26.10588f64.to_radians(), -2.62102f64.to_radians(),
-                                 0.724604, 88.35704f64.to_radians(),
-                                 0.00014f64.to_radians(), 0.983824);
-    println!("{}",x.to_degrees());
-    println!("{}",y.to_degrees());
-    println!("{}",z);
 
 }
