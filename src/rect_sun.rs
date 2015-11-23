d60a1000d783c1021d1b908bbbf84ca4908ce706
Saurav Sachidanand <sauravsachidanand@gmail.com>
2015-11-23 14:23:09 +0530

    Updated comments in rect_sun.rs

--- src/rect_sun.rs
+++ src/rect_sun.rs
@@ -8,18 +8,25 @@
 
 /*
 
-    Returns the geocentric rectangle coordinates of the sun.
+    rect_coord(sun_geocentric_longitude, sun_geocentric_latitude,
+               sun_radius_vector, mean_obliquity_of_ecliptic)
+               -> (x, y z)
     -----------------------------------------------------------------
-        geo_long_sun: The geocentric longitude of the sun
-         geo_lat_sun: The geocentric latitude of the sun
-         rad_vec_sun: The geocentric radius vector of the sun
-            mean_obl: The mean obliquity of the ecliptic
+    Returns the rectangle coordinates of the sun.
+
+    The origin of these coordinates is the center of the Earth.
+
+    The positive x-axis is directed towards the vernal equinox, that is,
+    longitude 0 degrees.
+    The positive y-axis lies in the plane of the equator and is directed
+    towards longitude 90 degrees.
+    The positive z-axis is directed towards the northern celestial pole.
 
 */
 
-pub fn rect_coord(geo_long_sun: f64, geo_lat_sun: f64, rad_vec_sun: f64, mean_obl: f64) -> (f64, f64, f64) {
-    let x = rad_vec_sun * geo_lat_sun.cos() * geo_long_sun.cos();
-    let y = rad_vec_sun * (geo_lat_sun.cos()*geo_long_sun.sin()*mean_obl.cos() - geo_lat_sun.sin()*mean_obl.sin());
-    let z = rad_vec_sun * (geo_lat_sun.cos()*geo_long_sun.sin()*mean_obl.sin() + geo_lat_sun.sin()*mean_obl.cos());
+pub fn rect_coord(sun_geo_long: f64, sun_geo_lat: f64, sun_rad_vec: f64, mean_obl: f64) -> (f64, f64, f64) {
+    let x = sun_rad_vec * sun_geo_lat.cos() * sun_geo_long.cos();
+    let y = sun_rad_vec * (sun_geo_lat.cos()*sun_geo_long.sin()*mean_obl.cos() - sun_geo_lat.sin()*mean_obl.sin());
+    let z = sun_rad_vec * (sun_geo_lat.cos()*sun_geo_long.sin()*mean_obl.sin() + sun_geo_lat.sin()*mean_obl.cos());
     (x, y, z)
 }
